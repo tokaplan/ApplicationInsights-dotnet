7b91abb20b7680e52073defdba863df967637022
Timothy Mothra Lee <tilee@microsoft.com>
2020-08-21 17:20:46 -0700

    self diagnostics file writer

---
+++ BASE/src/Microsoft.ApplicationInsights/Extensibility/Implementation/Tracing/SelfDiagnostics/SDEventListener.cs
@@ -0,0 +1,73 @@
+﻿namespace Microsoft.ApplicationInsights.Extensibility.Implementation.Tracing.SelfDiagnostics
+{
+    using System;
+    using System.Diagnostics;
+    using System.Diagnostics.Tracing;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+
+    using Microsoft.ApplicationInsights.Common.Extensions;
+
+    using static System.FormattableString;
+
+    internal class SDEventListener : EventListener
+    {
+        private readonly object lockObj = new object();
+        private readonly string logFilePath;
+        private readonly EventLevel level;
+
+        public SDEventListener(EventLevel level, string logFilePath)
+        {
+            this.level = level;
+            this.logFilePath = logFilePath;
+        }
+
+        protected override void OnEventWritten(EventWrittenEventArgs eventData)
+        {
+            var messageBody = eventData.Payload != null ?
+                string.Format(CultureInfo.CurrentCulture, eventData.Message, eventData.Payload.ToArray()) :
+                eventData.Message;
+
+            var message = Invariant($"{DateTime.UtcNow.ToInvariantString("o")}: {eventData.Level}: {messageBody}");
+
+            try
+            {
+                lock (this.lockObj)
+                {
+                    FileInfo file = new FileInfo(this.logFilePath);
+                    using (Stream stream = file.Open(FileMode.OpenOrCreate))
+                    {
+                        using (StreamWriter writer = new StreamWriter(stream))
+                        {
+                            stream.Position = stream.Length;
+                            writer.WriteLine(message);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // no op
+            }
+        }
+
+        /// <summary>
+        /// This method subscribes on Application Insights EventSource.
+        /// </summary>
+        /// <param name="eventSource">EventSource to subscribe to.</param>
+        protected override void OnEventSourceCreated(EventSource eventSource)
+        {
+            if (EventSources.ShouldSubscribe(eventSource))
+            {
+#if NET452
+                this.EnableEvents(eventSource, this.level, (EventKeywords)(-1));
+#else
+                this.EnableEvents(eventSource, this.level, EventKeywords.All);
+#endif
+            }
+
+            base.OnEventSourceCreated(eventSource);
+        }
+    }
+}
--- BASE/src/Microsoft.ApplicationInsights/Extensibility/Implementation/Tracing/SelfDiagnostics/SelfDiagnosticsFileWriter.cs
+++ BASE/src/Microsoft.ApplicationInsights/Extensibility/Implementation/Tracing/SelfDiagnostics/SelfDiagnosticsFileWriter.cs
@@ -1,10 +1,88 @@
 ﻿namespace Microsoft.ApplicationInsights.Extensibility.Implementation.Tracing.SelfDiagnostics
 {
-    internal class SelfDiagnosticsFileWriter : ISelfDiagnosticsFileWriter
+    using System;
+    using System.Diagnostics.Tracing;
+    using System.IO;
+
+    internal class SelfDiagnosticsFileWriter : ISelfDiagnosticsFileWriter, IDisposable
     {
+        private SDEventListener eventListener;
+        private bool disposedValue;
+
         public void Initialize(string level, string fileDirectory)
         {
-            // TODO: NEXT PR WILL IMPLEMENT THIS CLASS
+            string fileName = FileHelper.GenerateFileName();
+
+            if (!Enum.TryParse(level, out EventLevel eventLevel))
+            {
+                eventLevel = SelfDiagnosticsProvider.DefaultLevel;
+            }
+
+            if (IsValidLogsFolder(fileDirectory, fileName, out string fullLogPath)
+                || IsValidLogsFolder(SelfDiagnosticsProvider.DefaultDirectory, fileName, out fullLogPath))
+            {
+                WriteFileHeader(fullLogPath);
+                this.eventListener = new SDEventListener(eventLevel, fullLogPath);
+            }
+            else
+            {
+                // ERROR STATE
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            this.Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposedValue)
+            {
+                if (disposing)
+                {
+                    this.eventListener.Dispose();
+                }
+
+                this.disposedValue = true;
+            }
+        }
+
+        private static void WriteFileHeader(string logFilePath)
+        {
+            string[] lines =
+            {
+                ".NET SDK version: " + SdkVersionUtils.GetSdkVersion(string.Empty),
+                string.Empty,
+            };
+
+            File.WriteAllLines(logFilePath, lines);
+        }
+
+        private static bool IsValidLogsFolder(string fileDirectory, string fileName, out string fullLogPath)
+        {
+            bool result = false;
+            fullLogPath = null;
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(fileDirectory) && !string.IsNullOrWhiteSpace(fileName))
+                {
+                    string expandedDirectory = Environment.ExpandEnvironmentVariables(fileDirectory);
+                    var logsDirectory = new DirectoryInfo(expandedDirectory);
+                    FileHelper.TestDirectoryPermissions(logsDirectory);
+
+                    fullLogPath = Path.Combine(expandedDirectory, fileName);
+                    result = true;
+                }
+            }
+            catch (Exception)
+            {
+                // no op
+            }
+
+            return result;
         }
     }
 }
